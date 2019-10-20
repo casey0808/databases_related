686006c28074bf01ba3f523a3c3466f61f8b436d
Casey <casey_c0808@outlook.com>
2019-10-21 06:51:01 +0800

    Create trigger.sql

---
+++ MySQL/ig_clone/trigger.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE TRIGGER example_cannot_follow_self
+     BEFORE INSERT ON follows FOR EACH ROW
+     BEGIN
+          IF NEW.follower_id = NEW.following_id
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'You cannot follow yourself!';
+          END IF;
+     END;
+$$
+
+DELIMITER ;
