1b025dc4ec5ded59d97a2bb88c27cd339c1506ce
Casey <casey_c0808@outlook.com>
2019-10-21 07:00:33 +0800

    Create app.js

---
+++ MySQL/node_and_mysql/app.js
@@ -0,0 +1,67 @@
+var mysql = require('mysql');
+var faker = require('faker');
+
+var connection = mysql.createConnection({
+	host	: 'localhost',
+	user	: 'root',
+	database: 'join_us'
+});
+
+// // SELECTING DATA
+// var q = 'SELECT COUNT(*) AS total FROM users';
+// connection.query(q, function(error, results, fields){
+// 	if (error) throw error;
+// 	console.log(results[0].total);
+// });
+
+// var q = 'SELECT * FROM users';
+// connection.query(q, function(error, results, fields){
+// 	if (error) throw error;
+// 	console.log(results);
+//});
+
+// INSERTING DATA
+// var person = {
+// 	email: faker.internet.email(),
+// 	created_at: faker.date.past()
+// };
+
+// var end_result = connection.query('INSERT INTO users SET ?', person, 
+// 				function(error, results){
+// 					if (error) throw error;
+// 					console.log(results);
+// });
+
+// INSERTING LOTS OF DATA ===============
+var data = [];
+for (var i = 0; i < 500; i++){
+	data.push([
+		faker.internet.email(),
+		faker.date.past()
+	]);
+};
+
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+
+connection.query(q, [data], function(error, results){
+		if (error) throw error;
+		console.log(results);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connection.end();
