98391428119bec55dbde2b934b374d6cac4b3d61
Casey <casey_c0808@outlook.com>
2019-10-21 10:35:02 +0800

    Create unfollow_trigger.sql

---
+++ MySQL/ig_clone/unfollow_trigger.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE TRIGGER create_unfollow
+    AFTER DELETE ON follows FOR EACH ROW 
+BEGIN
+    INSERT INTO unfollows
+    SET follower_id = OLD.follower_id,
+        followee_id = OLD.followee_id;
+END$$
+
+DELIMITER ;
