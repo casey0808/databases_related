285b18a5f4749c7f06fd25e40f45d5f7ddd4be5b
Casey <casey_c0808@outlook.com>
2019-10-21 10:36:22 +0800

    Update ig_clone_data.sql

--- MySQL/ig_clone/ig_clone_data.sql
+++ MySQL/ig_clone/ig_clone_data.sql
@@ -44,6 +44,15 @@ CREATE TABLE follows (
     PRIMARY KEY(follower_id, followee_id)
 );
 
+CREATE TABLE unfollows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
 CREATE TABLE tags (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   tag_name VARCHAR(255) UNIQUE,
